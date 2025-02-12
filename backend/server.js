6a9bf3070ba55ef2aa0037651dd28f049d401634
Dinesh Yadav <13635627+HumbleBee14@users.noreply.github.com>
2025-02-12 11:43:14 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,33 @@ const balloonRoutes = require("./routes/balloonRoutes");
 const app = express();
 const PORT = process.env.PORT || 8001;
 
-app.use(cors());
-app.use(cors({ origin: "https://grepguru.com" }));
+// app.use(cors());
+app.use(cors({ origin: "*" }));
+
+// app.use(cors({ origin: "https://grepguru.com" }));
+
+
+/*
+const allowedOrigins = [
+  "https://grepguru.com", 
+  "https://windvoyager.pages.dev"
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+      } else {
+          callback(new Error("Not allowed by CORS"));
+      }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+*/
+
 app.use(express.json());
 
 // Routes
