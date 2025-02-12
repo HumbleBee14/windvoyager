0c34792d39af3ce6bb6aff3953f3f3618f043f0f
Dinesh Yadav <13635627+HumbleBee14@users.noreply.github.com>
2025-02-12 11:34:47 -0500

    Cloudflare deployment

--- frontend/services/balloonService.js
+++ frontend/services/balloonService.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const API_BASE_URL = "http://localhost:8001/api/balloons";
-const API_BASE_URL = "https://grepguru.com/windvoyager/api/balloons";
+// const API_BASE_URL = "https://grepguru.com/windvoyager/api/balloons";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8001/api/balloons";
 
 
 /**
